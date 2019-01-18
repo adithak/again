526af785613257cfdde3c14bcc1546ded756e826
adithak <adithak59@gmail.com>
2019-01-18 11:51:34 +0530

    Create mm.c

---
+++ mm.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+	char b[20];
+	int num;
+	printf("enter your string");
+	gets(b);
+	num=strlen(b);
+	while(num>=0)
+	{
+		printf("%c",b[n]);
+		num--;
+	}
+}
